5bdf5df76e02aab7539b5353f736e11e73092946
haymcarthur <haylee.mcarthur@familysearch.org>
2026-02-03 13:37:09 -0700

    Change port from 3002 to 3001 to avoid conflicts with user-test-hub

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3002
+    port: 3001,
+    open: false,
+    strictPort: true
+  },
+  preview: {
+    port: 3001
   }
 })
